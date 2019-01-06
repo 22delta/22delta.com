a9778319b5f33a6ced17ef3015b14e54e1840ccf
Francis Plaza <francis@plaza.ph>
2019-01-06 10:57:45 +0800

    Added JS easter egg

---
+++ static/assets/js/easter.js
@@ -0,0 +1,8 @@
+console.log("  ____  ____  ____  _____ _   _____  _\n" +
+" |___ \\|___ \\|  _ \\| ____| | |_   _|/ \\ \n" +
+"   __) | __) | | | |  _| | |   | | / _ \\ \n" +
+"  / __/ / __/| |_| | |___| |___| |/ ___ \\ \n" +
+" |_____|_____|____/|_____|_____|_/_/   \\_\\");
+
+console.log("\n\n%cCongratulations for coming this far, little one. You are a hackerman!",
+  'font-size: 18px; color: #3A5796');
